ba07277cf03cf0bef1bf35d3c42ecfcb082b3abd
Amebus <federico.ceruti@gmail.com>
2022-09-24 11:46:34 +0200

    eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,14 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
   'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
     '@vue/eslint-config-typescript',
+    '@vue/typescript/recommended',
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/eslint-recommended'
   ],
   overrides: [
     {
@@ -20,5 +22,30 @@ module.exports = {
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  ignorePatterns: [
+    'vue.config.js',
+		'**/tests/unit/*.ts',
+		'**/node_modules/**/*.*',
+		'**/src/**/*.json',
+		'**/src/**/*.svg',
+		'**/src/**/*.png',
+		'**/src/shims'
+  ],
+  rules: {
+    semi: 'off',
+    '@typescript-eslint/consistent-indexed-object-style': 'error',
+		'@typescript-eslint/consistent-type-definitions': 'error',
+		'@typescript-eslint/explicit-module-boundary-types': 'off',
+		'@typescript-eslint/member-delimiter-style': 'error',
+		'@typescript-eslint/no-duplicate-enum-values': 'error',
+		'@typescript-eslint/no-empty-function': 'warn',
+		'@typescript-eslint/no-explicit-any': 'off',
+		'@typescript-eslint/no-inferrable-types': 'off',
+		'@typescript-eslint/no-non-null-assertion': 'off',
+		'@typescript-eslint/no-this-alias': 'off',
+		'@typescript-eslint/semi': [
+			'warn'
+		],
   }
-}
+};
